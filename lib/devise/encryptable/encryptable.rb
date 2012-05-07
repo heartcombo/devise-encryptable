2ab79f23b59c5b61a8d4a543d0be8f7e77330dcb
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-07 17:49:41 -0300

    Added legacy encryptors and devise model

---
+++ lib/devise/encryptable/encryptable.rb
@@ -0,0 +1,27 @@
+module Devise
+  # Declare encryptors length which are used in migrations.
+  ENCRYPTORS_LENGTH = {
+    :sha1   => 40,
+    :sha512 => 128,
+    :clearance_sha1 => 40,
+    :restful_authentication_sha1 => 40,
+    :authlogic_sha512 => 128
+  }
+
+  # Used to define the password encryption algorithm.
+  mattr_accessor :encryptor
+  @@encryptor = nil
+
+  module Encryptable
+    module Encryptors
+      autoload :AuthlogicSha512, 'devise/encryptable/encryptors/authlogic_sha512'
+      autoload :Base, 'devise/encryptable/encryptors/base'
+      autoload :ClearanceSha1, 'devise/encryptable/encryptors/clearance_sha1'
+      autoload :RestfulAuthenticationSha1, 'devise/encryptable/encryptors/restful_authentication_sha1'
+      autoload :Sha1, 'devise/encryptable/encryptors/sha1'
+      autoload :Sha512, 'devise/encryptable/encryptors/sha512'
+    end
+  end
+end
+
+Devise.add_module(:encryptable, :model => 'devise/encryptable/model')
