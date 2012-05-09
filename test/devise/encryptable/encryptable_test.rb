ffb650c0f0ef0c414cacabb0dad3b3a28febcba5
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-09 08:17:06 -0300

    Fixing a test

--- test/devise/encryptable/encryptable_test.rb
+++ test/devise/encryptable/encryptable_test.rb
@@ -61,7 +61,7 @@ class EncryptableTest < ActiveSupport::TestCase
 
   test 'should generate a base64 hash using SecureRandom for password salt' do
     swap_with_encryptor Admin, :sha1 do
-      SecureRandom.expects(:base64).with(15).returns('01lI').twice
+      SecureRandom.expects(:base64).with(15).returns('01lI').once
       salt = create_admin.password_salt
       assert_not_equal '01lI', salt
       assert_equal 4, salt.size
