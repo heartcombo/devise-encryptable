742a2003a40fa51c49c8db1e60d7661105d106ca
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-09 08:14:44 -0300

    Removed a now useless devise configuration

--- test/rails_app/config/initializers/devise.rb
+++ test/rails_app/config/initializers/devise.rb
@@ -1,8 +1,6 @@
 Devise.setup do |config|
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
-  config.apply_schema = false
-
   require 'devise/orm/active_record'
 
   config.case_insensitive_keys = [ :email ]
