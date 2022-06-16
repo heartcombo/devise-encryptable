0bc8ea7db9fe08f7c81a7e83424f6dff2a587ba6
Jeff Trudeau <jeff.trudeau@gmail.com>
2022-06-16 15:04:15 -0400

    Changes per https://github.com/heartcombo/devise-encryptable/pull/21

--- lib/devise/encryptable/encryptors/pbkdf2.rb
+++ lib/devise/encryptable/encryptors/pbkdf2.rb
@@ -5,18 +5,18 @@ begin
         class Pbkdf2 < Base
           def self.compare(encrypted_password, password, stretches, salt, pepper)
             value_to_test = self.digest(password, stretches, salt, pepper)
-            ActiveSupport::SecurityUtils.fixed_length_secure_compare(encrypted_password, value_to_test)
+            Devise.secure_compare(encrypted_password, value_to_test)
           end
 
           def self.digest(password, stretches, salt, pepper)
-            hash = OpenSSL::Digest::SHA512.new
+            hash = OpenSSL::Digest.new('SHA512').new
             OpenSSL::KDF.pbkdf2_hmac(
-              password,
+              password.to_s,
               salt: "#{[salt].pack('H*')}#{pepper}",
               iterations: stretches,
               hash: hash,
               length: hash.digest_length,
-            ).unpack('H*')[0]
+            ).unpack1('H*')
           end
         end
       end
