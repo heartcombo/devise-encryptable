74db84b804a2ca1d6cfbc6d2c1a6dc211701ef16
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2026-01-06 12:20:52 -0300

    Remove check for Rails version >= 5

--- test/rails_app/config/environments/test.rb
+++ test/rails_app/config/environments/test.rb
@@ -9,13 +9,8 @@ RailsApp::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  if Rails.version >= "5"
-    config.public_file_server.enabled = true
-    config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
-  else
-    config.serve_static_files = true
-    config.static_cache_control = "public, max-age=3600"
-  end
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
